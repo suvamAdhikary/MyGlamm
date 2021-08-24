dddd0029266c8010af4267b9ac4d78458695a7cb
Suvam Adhikary <adhikary.saheb408@gmail.com>
2021-08-24 20:18:55 +0530

    fresh

---
+++ myglamm_clone/index.js
@@ -0,0 +1,200 @@
+
+        const myGlamm_products = [
+            {
+            name: "MYGLAMM SUPERFOODS ONION & MORINGA SHAMPOO",
+            description: "For Hair Fall Control",
+            price: "299",
+            images: ["https://files.myglamm.com/site-images/800x800/Product-1.jpg", "https://files.myglamm.com/site-images/800x800/1-1.jpg", "https://files.myglamm.com/site-images/800x800/2-1_3.jpg", "https://files.myglamm.com/site-images/800x800/3-1_2.jpg", "https://files.myglamm.com/site-images/800x800/4-1_2.jpg", "https://files.myglamm.com/site-images/800x800/5-1_2.jpg"],
+            rating: "4.7",
+            ratings: "7",
+            category: "HAIR CARE",
+            },
+            {
+                name: "MYGLAMM SUPERFOODS ONION & MORINGA HAIR OIL",
+                description: "For Hair Fall Control",
+                    price: "349",
+                    image: ["https://files.myglamm.com/site-images/200x200/Oil-Product.jpg", "https://files.myglamm.com/site-images/200x200/OIl-(1).jpg", "https://files.myglamm.com/site-images/200x200/OIl-(2).jpg", "https://files.myglamm.com/site-images/200x200/OIl-(3).jpg", "https://files.myglamm.com/site-images/200x200/OIl-(6).jpg", "https://files.myglamm.com/site-images/200x200/OIl-(5).jpg"],
+                    
+                    rating: "5",
+                ratings: "6",
+                category: "HAIR CARE",
+                
+                },
+                {
+                name: "MYGLAMM SUPERFOODS ONION & MORINGA SERUM",
+                description: "For Hair Fall Control",
+                    price: "399",
+                    image: ["https://files.myglamm.com/site-images/200x200/Serum-Product.jpg", "https://files.myglamm.com/site-images/200x200/Serum-(1).jpg", "https://files.myglamm.com/site-images/200x200/Serum-(2).jpg", "https://files.myglamm.com/site-images/200x200/Serum-(3).jpg", "https://files.myglamm.com/site-images/200x200/Serum-(6).jpg", "https://files.myglamm.com/site-images/200x200/Serum-(5).jpg"],
+                    
+                    rating: "5",
+                ratings: "3",
+                category: "HAIR CARE",
+                
+                },
+                {
+                name: "LIT CREAMY MATTE LIPSTICK - SANGRIA",
+                description: "Precision Perfect Matte Slimstick",
+                price: "595",
+                image:["https://files.myglamm.com/site-images/200x200/sangria-1-1.jpeg","https://files.myglamm.com/site-images/200x200/1627877761645-sangria-1.jpeg","https://files.myglamm.com/site-images/200x200/sangria-12.jpeg","https://files.myglamm.com/site-images/200x200/sangria-2-1.jpeg","https://files.myglamm.com/site-images/200x200/lit-creamy-matte-groupshot-5.jpeg"],
+                rating: "5",
+                ratings: "7",
+                category: "SKIN CARE",
+                },
+                {
+    
+                     name: "POSE HD BANANA POWDER - YELLOW",
+                description: "HD Yellow Setting Powder",
+                    price: "699",
+                    image: ["https://files.myglamm.com/site-images/200x200/pose-hd-banana-powder-2.jpeg", "https://i.ytimg.com/vi/EmfDLBs3oVk/hqdefault.jpg", "https://files.myglamm.com/site-images/200x200/pose-hd-banana-powder-8.jpeg", "https://files.myglamm.com/site-images/200x200/a46a1036.jpeg", "https://files.myglamm.com/site-images/200x200/bananapowder.jpeg", "https://files.myglamm.com/site-images/200x200/pose-hd-banana-powder-10.jpeg"],
+                rating: "4.8",
+                ratings: "97",
+                category: "SKIN CARE",
+                },
+                {
+                  name: "YOUTHFULL HYDRATING MOISTURISING CREAM WITH WATER BANK TECHNOLOGY",
+                description: "Lightweight Marine Hydrating Moisturising Cream",
+                    price: "1198",
+                    image: ["https://files.myglamm.com/site-images/200x200/1627822959419-Moistuirising-Cream-Swatch.jpeg", "https://files.myglamm.com/site-images/200x200/Moistuirising-Cream-Swatch_1.jpg", "https://files.myglamm.com/site-images/200x200/Moistuirisng-Cream-Open.jpg", "https://files.myglamm.com/site-images/200x200/Moistuirisng-Cream-Closed.jpg", "https://files.myglamm.com/site-images/200x200/vegan_105.jpeg"],
+                    
+                     rating: "5",
+                ratings: "8",
+                category: "SKIN CARE",
+                    
+                },
+                {
+                 name: "WIPEOUT GERM KILLING FACE WASH",
+                description: "Tea Tree Oil & Vitamin E-infused Germ-killing & Purifying Face Wash",
+                    price: "119",
+                    image: ["https://files.myglamm.com/site-images/200x200/1-1623082094122-Face-Wash-2-(1).jpg", "https://files.myglamm.com/site-images/200x200/FaceWash.jpg", "https://files.myglamm.com/site-images/200x200/PDP-ingredient-wipeout-tea-tree_3.jpg", "https://files.myglamm.com/site-images/200x200/PDP-ingredient-wipeout-Aleo.jpg", "https://files.myglamm.com/site-images/200x200/PDP-ingredient-wipeout-Cucumber_1.jpg"],
+                    rating: "4.9",
+                ratings: "105",
+                category: "SANITIZING CARE",
+                },
+                {
+                name: "MANISH MALHOTRA ANTIMICROBIAL SANITIZING HAND RUB",
+                description: "Oud Oil Nourishing Hand Sanitizer",
+                    price: "349",
+                    image: ["https://files.myglamm.com/site-images/200x200/MM-Hand-Rub-1.jpg", "https://files.myglamm.com/site-images/200x200/MM-Range-Short_2.jpg"],
+                    rating: "4.9",
+                ratings: "10",
+                category: "SANITIZING CARE",
+                },
+                {
+                  name: "WIPEOUT GERM KILLING BODY WASH",
+                description: "Tea Tree Oil-infused Germ-killing & Skin-regenerating Body Wash",
+                    price: "199",
+                    image: ["https://files.myglamm.com/site-images/200x200/Body-Wash-1.jpg", "https://files.myglamm.com/site-images/200x200/BodyWash.jpg", "https://files.myglamm.com/site-images/200x200/PDP-ingredient-wipeout-Chamomile.jpg", "https://files.myglamm.com/site-images/200x200/PDP-ingredient-wipeout-Lavender-Oil.jpg", "https://files.myglamm.com/site-images/200x200/Body-Wash-2_11.jpg"],
+                    rating: "5",
+                ratings: "46",
+                category: "SANITIZING CARE",
+                },
+    
+    
+                {
+                name: "POSE HD FOUNDATION STICK - WARM SAND",
+                description: "Nourishing, Matte, Skin-blurring Foundation",
+                    price: "599",
+                    image:["https://files.myglamm.com/site-images/200x200/WARM-SAND.jpg","https://i.ytimg.com/vi/iiS1Z3bRAL4/hqdefault.jpg","https://files.myglamm.com/site-images/200x200/WARM-SAND_1.jpg","https://files.myglamm.com/site-images/200x200/Standing-Closed-Product_3.jpg","https://files.myglamm.com/site-images/200x200/vegan_42.jpeg"],
+                rating: "4.7",
+                ratings: "3",
+                    category: "MAKEUP",
+                },
+    
+                {
+                  name: "JET SET EYES KAJAL EYELINER - NOIR",
+                description: "Long-wear Kohl Pencil",
+                    price: "750",
+                    image: ["https://files.myglamm.com/site-images/200x200/1627822347415-blackupdate.jpeg", "https://i.ytimg.com/vi/gnAZk01Jsuw/hqdefault.jpg", "https://files.myglamm.com/site-images/200x200/noir-4.jpeg", "https://files.myglamm.com/site-images/200x200/black-close1-update.jpeg", "https://files.myglamm.com/site-images/200x200/model-2.jpeg"],
+                    rating: "4.9",
+                ratings: "117",
+                    category: "MAKEUP",
+                },
+                {
+                 name: "K.PLAY MANDARIN BRIGHTENING SHEET MASK",
+                description: "Mandarin-infused Brightening Sheet Mask",
+                    price: "145",
+                    image: ["https://files.myglamm.com/site-images/200x200/mandarin-brightening-sheet-mask-copy.jpeg", "https://files.myglamm.com/site-images/200x200/mandarin.jpeg", "https://files.myglamm.com/site-images/200x200/kplay-mandarin-brightening-sheet-mask-1.jpeg", "https://files.myglamm.com/site-images/200x200/sheet-mask-2.jpeg"],
+                    
+                    rating: "4.8",
+                ratings: "36",
+                    category: "MAKEUP",
+    },
+        ];
+            
+        const myGlamm_products_json = JSON.stringify(myGlamm_products);
+        localStorage.setItem("glamm_products", myGlamm_products_json);
+
+
+let myProducts = localStorage.getItem('glamm_products');
+
+myProducts = JSON.parse(myProducts);
+
+
+let data_div = document.getElementById("home__products");
+function showProducts() {
+
+    
+    
+    myProducts.forEach(function(product, n) {
+
+        let div = document.createElement('div');
+        div.onclick = function () {
+                window.location.assign(`products/${n}.html`);
+        }
+
+        let p_name = document.createElement('p');
+        p_name.innerText = product.name;
+
+        let p_description = document.createElement('p');
+        p_description.innerText = product.description;
+
+        let p_price = document.createElement('p');
+        p_price.innerText = '₹ ' + product.price;
+
+        let image = document.createElement('img');
+        let temp = product.images;
+        image.src = temp;
+        console.log(temp);
+
+        // let btn = document.createElement('button');
+        // btn.setAttribute("class", "addtoCart");
+        // btn.innerText = 'Add to Cart';
+        // btn.onclick = function () {
+        //     addtoCart(product);
+        // }
+
+        div.append(p_name, p_description, p_price);
+
+        data_div.append(div);
+
+    })
+}
+showProducts()
+
+// if(localStorage.getItem("bag") === null) {
+//     localStorage.setItem("bag", JSON.stringify([]));
+// }
+
+// let check = [];
+
+// function addtoCart(p) {
+
+//     let cart_data = JSON.parse(localStorage.getItem("bag"));
+
+//     if(check.includes(p.name) == true) {
+//         alert("Product is Already in The Cart.");
+        
+//         for(let i = 0; i < cart_data.length; i++) {
+//             if(cart_data[i].name === p.name) {
+//                 +cart_data[i].quantity++;
+//             }
+//         }
+
+//     } else {
+//         check.push(p.name)
+//         p.quantity = 1;
+//         cart_data.push(p);
+//     }
+
+//     localStorage.setItem("bag", JSON.stringify(cart_data));
+// }
