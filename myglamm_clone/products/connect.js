5d7bd169f31e70c81f4bb10bcdccf37b6a986245
Suvam Adhikary <adhikary.saheb408@gmail.com>
2021-08-25 15:02:37 +0530

    nav bar, search bar, connectivity between pages

---
+++ myglamm_clone/products/connect.js
@@ -0,0 +1,30 @@
+let makeup = document.getElementById('makeup__link');
+makeup.href = '../makeup.html'
+
+let hairCare = document.getElementById('hair__care');
+hairCare.href = '../hairCare.html';
+
+let skinCare = document.getElementById('skin__care');
+skinCare.href = '../skinCare.html';
+
+let sanitizingCare = document.getElementById('sanitizing__care');
+sanitizingCare.href = '../sanitizingCare.html';
+
+let collection = document.getElementById('collection');
+collection.href = '../collection.html';
+
+let rewards = document.getElementById('rewards');
+rewards.href = '../rewards.html';
+
+let myglammStudio = document.getElementById('myglamm__studio');
+myglammStudio.href = '../myglammStudio.html';
+
+let mainLogo = document.getElementById('main__logo');
+mainLogo.href = '../index.html';
+
+// let searchBar = document.getElementById('search__bar');
+
+let bagLink = document.getElementById('bag__link');
+bagLink.href = '../cart.html';
+
+// let profile = document.getElementById('profile');
