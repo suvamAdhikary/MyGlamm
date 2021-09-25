2bbce0db3e466fbcaa0c6131ed3fd34999642c3a
Rajat Rana <rajatrana.rk@gmail.com>
2021-09-25 17:18:51 +0530

    changes

---
+++ myglamm_clone/javascript/about.js
@@ -0,0 +1,117 @@
+import {htmlfileAbout} from "./aboutExport.js";
+
+
+let main = document.getElementById("aboutMain");
+
+
+main.innerHTML = (htmlfileAbout());
+
+
+var mainBackground = document.getElementById("mainAboutDiv");
+          var firstBtn = document.getElementById("firstButton");
+          var secondBtn = document.getElementById("secondButton");
+          var thirdBtn = document.getElementById("thirdButton");
+          var fourthBtn = document.getElementById("fourthButton");
+          var p1 = document.getElementById("paragraph1");
+          var p2 = document.getElementById("paragraph2");
+          var p3 = document.getElementById("paragraph3");
+          var p4 = document.getElementById("paragraph4");
+          var l1 = document.getElementById("list1");
+          var l2 = document.getElementById("list2");
+          var l3 = document.getElementById("list3");
+          var l4 = document.getElementById("list4");
+  
+  
+  
+          fourthBtn.addEventListener("click", function(){
+                      mainBackground.style.backgroundImage = "linear-gradient(rgba(0, 0, 0, .5), rgba(0, 0, 0, .5)),url('https://s3-ap-southeast-1.amazonaws.com/myglamm.com/content/file-1505123624624-blobid1505123691780.jpg')";
+                      
+                      fourthBtn.style.border = "none";
+                      thirdBtn.style.border = "none";
+                      secondBtn.style.border = "none";
+                      firstBtn.style.border = "none";
+                      fourthBtn.style.border = "2.5px solid rgb(255,20,147,0.4)";
+                      p1.style.display = "none";
+                      p2.style.display = "none";
+                      p3.style.display = "none";
+                      p4.style.display = "none";
+                      p1.style.display = "block";
+                      list1.style.color = "rgb(255, 255, 255,0.6)";
+  
+                      list2.style.color = "rgb(255, 255, 255,0.6)";
+                      list3.style.color = "rgb(255, 255, 255,0.6)";
+                      list4.style.color =  "rgb(255, 255, 255,0.6)";
+                      list4.style.color = "white";
+                       
+                     
+                  })
+  
+          thirdBtn.addEventListener("click", function(){
+                      mainBackground.style.backgroundImage = "linear-gradient(rgba(0, 0, 0, .5), rgba(0, 0, 0, .5)),url('https://s3-ap-southeast-1.amazonaws.com/myglamm.com/content/file-1505123709999-blobid1505123775162.jpg')";
+                      fourthBtn.style.border = "none";
+                      thirdBtn.style.border = "none";
+                      secondBtn.style.border = "none";
+                      firstBtn.style.border = "none";
+                      thirdBtn.style.border = "2.5px solid rgb(255,20,147,0.4)";
+  
+                      p1.style.display = "none";
+                      p2.style.display = "none";
+                      p3.style.display = "none";
+                      p4.style.display = "none";
+                      p2.style.display = "block";
+  
+                      list1.style.color = "rgb(255, 255, 255,0.6)";
+  
+                      list2.style.color = "rgb(255, 255, 255,0.6)";
+                      list3.style.color = "rgb(255, 255, 255,0.6)";
+                      list4.style.color =  "rgb(255, 255, 255,0.6)";
+                      list3.style.color = "white";
+  
+                  })
+  
+          secondBtn.addEventListener("click", function(){
+                      mainBackground.style.backgroundImage = "linear-gradient(rgba(0, 0, 0, .5), rgba(0, 0, 0, .5)),url('https://s3-ap-southeast-1.amazonaws.com/myglamm.com/content/file-1505124062574-blobid1505124131185.jpg')";
+                      fourthBtn.style.border = "none";
+                      thirdBtn.style.border = "none";
+                      secondBtn.style.border = "none";
+                      firstBtn.style.border = "none";
+                      secondBtn.style.border = "2.5px solid rgb(255,20,147,0.4)";
+  
+                      p1.style.display = "none";
+                      p2.style.display = "none";
+                      p3.style.display = "none";
+                      p4.style.display = "none";
+                      p3.style.display = "block";
+  
+                      list1.style.color = "rgb(255, 255, 255,0.6)";
+  
+                      list2.style.color = "rgb(255, 255, 255,0.6)";
+                      list3.style.color = "rgb(255, 255, 255,0.6)";
+                      list4.style.color =  "rgb(255, 255, 255,0.6)";
+                      list2.style.color = "white";
+                  })
+  
+          firstBtn.addEventListener("click", function(){
+                      mainBackground.style.backgroundImage = "linear-gradient(rgba(0, 0, 0, .5), rgba(0, 0, 0, .5)),url('https://s3-ap-southeast-1.amazonaws.com/myglamm.com/content/file-1505124076895-blobid1505124145750.jpg')";
+                      fourthBtn.style.border = "none";
+                      thirdBtn.style.border = "none";
+                      secondBtn.style.border = "none";
+                      firstBtn.style.border = "none";
+                      firstBtn.style.border = "2.5px solid rgb(255,20,147,0.4)";
+  
+                      p1.style.display = "none";
+                      p2.style.display = "none";
+                      p3.style.display = "none";
+                      p4.style.display = "none";
+                      p4.style.display = "block";
+  
+                      list1.style.color = "rgb(255, 255, 255,0.6)";
+  
+                      list2.style.color = "rgb(255, 255, 255,0.6)";
+                      list3.style.color = "rgb(255, 255, 255,0.6)";
+                      list4.style.color =  "rgb(255, 255, 255,0.6)";
+                      list1.style.color = "white";
+                      
+                  })
+    
+    
---
+++ myglamm_clone/javascript/aboutExport.js
@@ -0,0 +1,47 @@
+  
+       function htmlfileAbout(){
+
+        return (`
+        <div id="mainAboutDiv">
+        <button id="firstButton">EXPERT ADVICE ANYTIME, ANYWHERE <br><li id="list1"></li> </button>
+        <button id="secondButton">GUILT FREE GLAMOUR <br><li id="list2"></li></button>
+        <button id = "thirdButton">INTERNATIONAL PRODUCTS THAT MULTITASKS <br> <li id="list3"></li></button>
+        <button id="fourthButton">ABOUT THE BRAND <br> <li id="list4"></li></button>
+          
+        <div id="paragraph1">
+          <h3>ABOUT THE</h3>  
+          <h1>BRAND</h1>
+          <p>Whether you are in New York, Mumbai or Milan we know for all you beautiful women, most days are packed with countless plans and endless to-do lists. Friends, family, college and even colleagues vying for your time. And while you juggle all this effortlessly, we know that often you're left with barely any time for yourself.
+
+              Regardless of how busy the day may be, you shouldn't have to compromise on the way you want to look or on your glam quotient. Which is why, MyGlamm backed by one of Europe’s largest natural beauty companies, collaborated with global experts and makeup artists to bring about exciting innovations in makeup to accomplish our single, focused goal – make looking glamorous effortless!</p>
+        </div>
+
+        <div id="paragraph2">
+          <h3>INTERNATIONAL PRODUCTS THAT</h3>  
+          <h1>MULTITASK</h1>
+          <p>Innovation for ease is at the heart of our products. Conceptualised, formulated and developed in laboratories in Italy and Germany, MyGlamm products have been created to simplify a woman's makeup routine.
+              
+              Our multi functional products have used innovation to not only reduce the number of products you need to carry in your handbag but also cut down the number of steps you need to follow while applying makeup. While with our single use products we have created new textures, precise applicators, and never seen before formats which simplify and reduce the amount of time you need to do your makeup.
+              
+              So whether you want a wheel applicator for your eyelids, a mesmerising powder in a pencil or five of your face products in one, we are making sure that makeup is the last thing you have to worry about.</p>
+        </div>
+
+        <div id = "paragraph3">
+          <h3>GUILT-FREE</h3>  
+          <h1>GLAMOUR</h1>
+          <p>100% cruelty free, the MyGlamm makeup line is enriched with special minerals, vitamins and oils. The European formulations of our products use the finest ingredients that both nature and science have to offer. Ingredients like Jojoba oil, Argan oil, Tropical Plant oil nourish the skin while our advanced formulations deliver a long lasting, smudge free, rich colour pay off to make you look glamorous all day long. Easy to use and carry, they are bound to be purse staples; your go-to makeup essentials for high impact makeup up on the go.</p>
+
+        </div>
+
+        <div id="paragraph4">
+          <h3>EXPERT ADVICE, ANYTIME</h3>  
+          <h1>ANYWHERE</h1>
+          <p>It’s always great to pick up a nifty makeup tip or several isn’t it? Using cutting edge technology, we have created many engaging experiences for you. From LIVE tutorials, expert chats to masterclasses with the world’s best makeup artists along with unique makeup hangouts, we have brought together the best of the makeup world under one roof. Not only that, we have taken our commitment of ‘making glamour accessible to every woman’ a notch higher by bringing makeup tutorials right to your doorstep. Just use our at home service and our makeup trainers will teach you the tricks to look glamorous.</p>
+
+        </div>
+  </div>
+  `);      
+}
+
+
+export {htmlfileAbout};
